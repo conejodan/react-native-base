802b3bf1be81eb214546d14be9fb1697b0212983
Daniel Sanchez <daniel.sanchez@apscreativas.com>
2018-04-12 10:17:53 -0500

    Agregando Spinner y texto de Cargando cuando inicie la app

--- App.js
+++ App.js
@@ -4,6 +4,7 @@
 import React, {Component} from 'react';
 import {View,Text} from 'react-native';
 import Application from './src/Application';
+import {Spinner} from 'native-base';
 
 export default class App extends Component {
   state = {loaded:false};
@@ -14,12 +15,16 @@ export default class App extends Component {
       'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
       'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
     });
-    this.setState({loaded:true});
+    const that = this;
+    setTimeout(function(){
+      that.setState({loaded:true});
+    },1000);
+    
   }
 
   render() {
     if(!this.state.loaded){
-      return <View><Text>Loading</Text></View>
+      return <View style={{flex:1, justifyContent:'center', alignItems:'center', flexDirection:'column'}}><Spinner color="blue"/><Text style={{fontSize: 20,fontWeight: 'bold'}}>Cargando...</Text></View>
     }
     return (
        <Application/>
